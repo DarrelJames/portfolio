4830181cde5ac618a5594dbaf8da861927bc4706
Darrel Castellano <dcastellano2007@gmail.com>
2020-05-12 11:54:18 -0700

    Boiler Plate for pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import Header from "./components/Header/Header";
+import Projects from "./components/Projects/Projects";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <About />
+      <Contact />
+      <Header />
+      <Projects />
+    </div>
+  );
 }
 
 export default App;
