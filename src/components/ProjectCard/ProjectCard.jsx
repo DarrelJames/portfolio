1bb2e57cbf149a28cbf0050526f44e2d704eb20f
Darrel Castellano <dcastellano2007@gmail.com>
2020-05-17 11:20:53 -0700

    Add project card component

---
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import ReactCardFlip from "react-card-flip";
+import styles from "./ProjectCard.module.css";
+
+const ProjectCard = ({ image }) => {
+  const [isFlipped, setIsFlipped] = useState(false);
+  return (
+    <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+      <div className={styles.card} key="front">
+        <img
+          src={image}
+          alt="covid19"
+          onClick={() => setIsFlipped(!isFlipped)}
+        />
+        <div className={styles.cardContent}>
+          <div className={styles.header}>
+            <span
+              className={styles.title}
+              onClick={() => setIsFlipped(!isFlipped)}
+            >
+              Covid19 Tracker
+            </span>
+            <i class="fas fa-ellipsis-v"></i>
+          </div>
+          <p>Keep track of the covid 19 numbers around the world.</p>
+        </div>
+      </div>
+
+      <div className={styles.card} key="back">
+        <div className={styles.cardContent}>
+          <div
+            className={styles.header}
+            onClick={() => setIsFlipped(!isFlipped)}
+          >
+            <h4>Accomplisments</h4>
+            <i class="fas fa-times"></i>
+          </div>
+          <ul>
+            <li>React Framework</li>
+            <li>React Framework</li>
+            <li>React Framework</li>
+            <li>React Framework</li>
+          </ul>
+        </div>
+      </div>
+    </ReactCardFlip>
+  );
+};
+
+export default ProjectCard;
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import ProjectCard from "../ProjectCard/ProjectCard";
 import styles from "./Projects.module.css";
+
+import covid from "../../images/covid.png";
 const Projects = () => {
   return (
     <>
       <div className={styles.container} id="projects">
         <h1>Projects</h1>
+        <div className={styles.projects}>
+          <ProjectCard image={covid} />
+          <ProjectCard image={covid} />
+          <ProjectCard image={covid} />
+          <ProjectCard image={covid} />
+        </div>
       </div>
     </>
   );
